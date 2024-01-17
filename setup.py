fa2ce2db063053616d39533215dbd9041f74d59c
Dhruv Soni <soni.dhr@northeastern.edu>
2024-01-17 11:59:56 -0500

    Setup py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    Read requirements.txt file and return list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.read()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+setup(
+    name='MLProject',
+    version='0.1',
+    author='Dhruv',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
